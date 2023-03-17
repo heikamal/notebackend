1086aaaa87a8242d2e8cf508b17a04a6d4c4c878
Heikki Malkavaara <heimal@student.uef.fi>
2023-03-17 11:18:26 +0200

    validointipäivitystä

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const cors = require('cors')
 const app = express()
 const Note = require('./models/note')
 
-
 app.use(cors())
-app.use(express.json())
 app.use(express.static('build'))
+app.use(express.json())
+
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -16,7 +16,6 @@ const requestLogger = (request, response, next) => {
     console.log('---')
     next()
 }
-
 app.use(requestLogger)
 
 let notes = [
@@ -47,35 +46,35 @@ app.get('/api/notes', (req, res) => {
       })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id).then(note => {
-      response.json(note)
+        if (note) {
+            response.json(note)
+        } else {
+            response.status(404).end()
+        }   
     })
+    .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id)
-    notes = notes.filter(note => note.id !== id)
-
-    res.status(204).end()
+app.delete('/api/notes/:id', (req, res, next) => {
+    Note.findByIdAndRemove(req.params.id)
+    .then(result => {
+        res.status(204).end()
+    }).catch(error => next(error))
 })
 
-const generateId = () => {
+/*const generateId = () => {
     const maxId = notes.length > 0 
         ? Math.max(...notes.map(n => n.id)) 
         : 0
 
     return maxId + 1
-}
+}*/
 
 app.post('/api/notes', (req, res) => {
     
     const body = req.body
-    if (body.content === undefined) {
-        return res.status(400).json({
-            error: 'content missing'
-        })
-    }
 
     const note = new Note({
         content: body.content,
@@ -84,12 +83,40 @@ app.post('/api/notes', (req, res) => {
 
     note.save().then(savedNote => {
         res.json(savedNote)
-    })
+    }).catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (req, res, next) => {
+    const { content, important } = req.body
+
+    Note.findByIdAndUpdate(
+        req.params.id,
+        { content, important }, 
+        { new: true, runValidators: true, context: 'query' })
+    .then(updatedNote => {
+        res.json(updatedNote)
+    }).catch(error => next(error))
 })
 
 const unkownEndpoint = (request, response) => {
     response.status(404).send({ error: 'uknown endpoint' })
 }
+
+app.use(unkownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message})
+    }
+  
+    next(error)
+}
+
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 3001
